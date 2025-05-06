e4fb9026e6efd6d65be82f0b55669bf4358a2fb3
Gowni Gnanendra Reddy <140694819+gnana333@users.noreply.github.com>
2025-05-06 19:23:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing",{
-    strings:[" Btech Student"," Entry Level Programmer", "Web Designer",],
+    strings:[" Btech Graduate"," Entry Level Programmer", "Web Designer", "Developer",],
     typeSpeed:100,
     backSpeed:60,
     loop:true
